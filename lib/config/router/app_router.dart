5b9956f89abeeec43c4292b652027e94d6cacd0c
joseperu2503 <joseperu2503@gmail.com>
2024-03-29 01:56:21 -0500

    avance tabs

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -2,44 +2,96 @@ import 'package:delivery_app/features/auth/providers/auth_provider.dart';
 import 'package:delivery_app/features/auth/screens/home_screen.dart';
 import 'package:delivery_app/features/auth/screens/login_screen.dart';
 import 'package:delivery_app/features/dashboard/screens/dashboard_screen.dart';
+import 'package:delivery_app/features/order/screens/order_screen.dart';
+import 'package:delivery_app/features/shared/widgets/tabs.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 import 'app_router_notifier.dart';
 
+final GlobalKey<NavigatorState> _rootNavigatorKey = GlobalKey<NavigatorState>();
+final GlobalKey<NavigatorState> _tabNavigatorKey = GlobalKey<NavigatorState>();
+
 final goRouterProvider = Provider<GoRouter>((ref) {
   final goRouterNotifier = ref.read(goRouterNotifierProvider);
 
+  redirect(BuildContext context, GoRouterState state) {
+    final authStatus = goRouterNotifier.authStatus;
+    final isGoingTo = state.fullPath;
+
+    if (authStatus == AuthStatus.authenticated) {
+      return '/dashboard';
+    }
+
+    if (authStatus == AuthStatus.notAuthenticated) {
+      if (isGoingTo == '/login' || isGoingTo == '/home') return null;
+      return '/home';
+    }
+
+    return null;
+  }
+
   return GoRouter(
+    navigatorKey: _rootNavigatorKey,
     initialLocation: '/home',
     refreshListenable: goRouterNotifier,
-    redirect: (context, state) {
-      final authStatus = goRouterNotifier.authStatus;
-      final isGoingTo = state.fullPath;
-
-      if (authStatus == AuthStatus.authenticated) {
-        return '/dashboard';
-      }
-
-      if (authStatus == AuthStatus.notAuthenticated) {
-        if (isGoingTo == '/login' || isGoingTo == '/home') return null;
-        return '/home';
-      }
-
-      return null;
-    },
+    // redirect: redirect,
     routes: [
+      StatefulShellRoute.indexedStack(
+        parentNavigatorKey: _rootNavigatorKey,
+        builder: (context, state, navigationShell) {
+          return Tabs(
+            navigationShell: navigationShell,
+          );
+        },
+        branches: [
+          StatefulShellBranch(
+            navigatorKey: _tabNavigatorKey,
+            routes: [
+              GoRoute(
+                path: '/dashboard',
+                builder: (context, state) => const DashboardScreen(),
+                parentNavigatorKey: _tabNavigatorKey,
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                path: '/order',
+                builder: (context, state) => const OrderScreen(),
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                path: '/favorites',
+                builder: (context, state) => const DashboardScreen(),
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                path: '/profile',
+                builder: (context, state) => const DashboardScreen(),
+              ),
+            ],
+          ),
+        ],
+      ),
       GoRoute(
         path: '/home',
         builder: (context, state) => const HomeScreen(),
+        parentNavigatorKey: _rootNavigatorKey,
+        redirect: redirect,
       ),
       GoRoute(
         path: '/login',
         builder: (context, state) => const LoginScreen(),
-      ),
-      GoRoute(
-        path: '/dashboard',
-        builder: (context, state) => const DashboardScreen(),
+        parentNavigatorKey: _rootNavigatorKey,
       ),
     ],
   );
---
+++ lib/features/shared/widgets/tabs.dart
@@ -0,0 +1,116 @@
+import 'package:delivery_app/config/constants/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
+
+// https://github.com/flutter/packages/blob/main/packages/go_router/example/lib/stateful_shell_route.dart
+
+class Tabs extends StatelessWidget {
+  const Tabs({super.key, required this.navigationShell});
+
+  final StatefulNavigationShell navigationShell;
+
+  void _onTap(BuildContext context, int index) {
+    navigationShell.goBranch(
+      index,
+      initialLocation: index == navigationShell.currentIndex,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: navigationShell,
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: AppColors.white,
+        currentIndex: navigationShell.currentIndex,
+        onTap: (value) {
+          _onTap(context, value);
+        },
+        // unselectedLabelStyle: TextStyle(
+        //   fontSize: 14,
+        //   fontWeight: FontWeight.w400,
+        //   color: AppColors.gray400,
+        // ),
+        // selectedLabelStyle: TextStyle(
+        //   fontSize: 14,
+        //   fontWeight: FontWeight.w400,
+        //   color: AppColors.gray400,
+        // ),
+        unselectedItemColor: AppColors.gray400,
+        selectedItemColor: AppColors.primary500,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: SvgPicture.asset(
+              'assets/icons/tabs/home_outlined.svg',
+              colorFilter: const ColorFilter.mode(
+                AppColors.gray400,
+                BlendMode.srcIn,
+              ),
+            ),
+            activeIcon: SvgPicture.asset(
+              'assets/icons/tabs/home_solid.svg',
+              colorFilter: const ColorFilter.mode(
+                AppColors.primary500,
+                BlendMode.srcIn,
+              ),
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: 'My Order',
+            icon: SvgPicture.asset(
+              'assets/icons/tabs/cart_outlined.svg',
+              colorFilter: const ColorFilter.mode(
+                AppColors.gray400,
+                BlendMode.srcIn,
+              ),
+            ),
+            activeIcon: SvgPicture.asset(
+              'assets/icons/tabs/cart_solid.svg',
+              colorFilter: const ColorFilter.mode(
+                AppColors.primary500,
+                BlendMode.srcIn,
+              ),
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: 'Favorite',
+            icon: SvgPicture.asset(
+              'assets/icons/tabs/cart_outlined.svg',
+              colorFilter: const ColorFilter.mode(
+                AppColors.gray400,
+                BlendMode.srcIn,
+              ),
+            ),
+            activeIcon: SvgPicture.asset(
+              'assets/icons/tabs/cart_solid.svg',
+              colorFilter: const ColorFilter.mode(
+                AppColors.primary500,
+                BlendMode.srcIn,
+              ),
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: 'Profile',
+            icon: SvgPicture.asset(
+              'assets/icons/tabs/cart_outlined.svg',
+              colorFilter: const ColorFilter.mode(
+                AppColors.gray400,
+                BlendMode.srcIn,
+              ),
+            ),
+            activeIcon: SvgPicture.asset(
+              'assets/icons/tabs/cart_solid.svg',
+              colorFilter: const ColorFilter.mode(
+                AppColors.primary500,
+                BlendMode.srcIn,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
