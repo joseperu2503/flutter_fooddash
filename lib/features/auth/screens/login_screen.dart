47055c03f14dc68e8efaa48b18a36fca73b53221
joseperu2503 <joseperu2503@gmail.com>
2024-06-12 15:28:31 -0500

    login

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -29,190 +29,211 @@ class LoginScreenState extends ConsumerState<LoginScreen> {
 
     return Container(
       color: AppColors.background,
-      child: Stack(
-        children: [
-          Positioned(
-            top: -40,
-            left: -55,
-            child: Container(
-              width: 140,
-              height: 140,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: AppColors.white,
-                border: Border.all(
-                  color: AppColors.primary,
-                  width: 36,
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            top: -90,
-            left: 0,
-            child: Container(
-              width: 175,
-              height: 175,
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                color: Color(0xffFFECE7),
-              ),
-            ),
-          ),
-          Positioned(
-            top: -109,
-            right: -90,
-            child: Container(
-              width: 200,
-              height: 200,
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                color: AppColors.primary,
-              ),
-            ),
-          ),
-          Scaffold(
-            backgroundColor: Colors.transparent,
-            body: CustomScrollView(
-              physics: const ClampingScrollPhysics(),
-              slivers: [
-                SliverAppBar(
-                  automaticallyImplyLeading: false,
-                  backgroundColor: Colors.transparent,
-                  systemOverlayStyle: SystemUiOverlayStyle.dark,
-                  toolbarHeight: 60,
-                  flexibleSpace: Stack(
-                    children: [
-                      SafeArea(
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 24,
-                            vertical: 8,
-                          ),
-                          height: 60,
-                          child: const Row(
-                            children: [
-                              CustomBackButton(),
-                            ],
-                          ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: CustomScrollView(
+          physics: const ClampingScrollPhysics(),
+          slivers: [
+            SliverAppBar(
+              automaticallyImplyLeading: false,
+              backgroundColor: Colors.white,
+              toolbarHeight: 60,
+              pinned: true,
+              scrolledUnderElevation: 0,
+              flexibleSpace: Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  Positioned(
+                    top: -40,
+                    left: -55,
+                    child: Container(
+                      width: 140,
+                      height: 140,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: AppColors.white,
+                        border: Border.all(
+                          color: AppColors.primary,
+                          width: 36,
                         ),
                       ),
-                    ],
-                  ),
-                ),
-                SliverToBoxAdapter(
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 24,
                     ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                  ),
+                  Positioned(
+                    top: -90,
+                    left: 0,
+                    child: Container(
+                      width: 175,
+                      height: 175,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color(0xffFFECE7),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: -109,
+                    right: -90,
+                    child: Container(
+                      width: 200,
+                      height: 200,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: AppColors.primary,
+                      ),
+                    ),
+                  ),
+                  SafeArea(
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 24,
+                        vertical: 8,
+                      ),
+                      height: 60,
+                      child: const Row(
+                        children: [
+                          CustomBackButton(),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 24,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        const SizedBox(
-                          height: 105,
-                        ),
-                        const Text(
-                          'Login',
+                        Text(
+                          'Food',
                           style: TextStyle(
-                            fontSize: 36,
+                            fontSize: 24,
                             fontWeight: FontWeight.w600,
-                            color: AppColors.gray900,
+                            color: AppColors.primary,
                             height: 43 / 36,
                             leadingDistribution: TextLeadingDistribution.even,
                           ),
                         ),
-                        const SizedBox(
-                          height: 50,
-                        ),
-                        const Text(
-                          'E-mail',
+                        Text(
+                          'Dash',
                           style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w400,
-                            color: AppColors.label,
-                            height: 16 / 16,
+                            fontSize: 24,
+                            fontWeight: FontWeight.w600,
+                            color: AppColors.input,
+                            height: 43 / 36,
                             leadingDistribution: TextLeadingDistribution.even,
                           ),
-                        ),
-                        const SizedBox(
-                          height: 12,
-                        ),
-                        CustomTextField(
-                          hintText: 'Your email',
-                          value: loginState.email,
-                          onChanged: (value) {
-                            ref.read(loginProvider.notifier).changeEmail(value);
-                          },
-                          textInputAction: TextInputAction.next,
-                          keyboardType: TextInputType.emailAddress,
-                        ),
-                        const SizedBox(
-                          height: 29,
-                        ),
-                        const Text(
-                          'Password',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w400,
-                            color: AppColors.label,
-                            height: 16 / 16,
-                            leadingDistribution: TextLeadingDistribution.even,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 12,
-                        ),
-                        CustomTextField(
-                          hintText: 'Your password',
-                          value: loginState.password,
-                          onChanged: (value) {
-                            ref
-                                .read(loginProvider.notifier)
-                                .changePassword(value);
-                          },
-                          textInputAction: TextInputAction.done,
-                          onFieldSubmitted: (value) {
-                            ref.read(loginProvider.notifier).login();
-                          },
-                        ),
-                        const SizedBox(
-                          height: 32,
-                        ),
-                        const Center(
-                          child: Text(
-                            'Forgot password?',
-                            style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w400,
-                              color: AppColors.primary,
-                              height: 1,
-                              leadingDistribution: TextLeadingDistribution.even,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 32,
-                        ),
-                        Center(
-                          child: CustomButton(
-                            onPressed: () {
-                              ref.read(loginProvider.notifier).login();
-                            },
-                            text: 'LOGIN',
-                            boxShadow: BoxShadowType.gray,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 50,
                         ),
                       ],
                     ),
-                  ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    const Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.w600,
+                        color: AppColors.gray900,
+                        height: 43 / 36,
+                        leadingDistribution: TextLeadingDistribution.even,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 31,
+                    ),
+                    const Text(
+                      'E-mail',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                        color: AppColors.label,
+                        height: 16 / 16,
+                        leadingDistribution: TextLeadingDistribution.even,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    CustomTextField(
+                      hintText: 'Your email',
+                      value: loginState.email,
+                      onChanged: (value) {
+                        ref.read(loginProvider.notifier).changeEmail(value);
+                      },
+                      textInputAction: TextInputAction.next,
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    const SizedBox(
+                      height: 29,
+                    ),
+                    const Text(
+                      'Password',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                        color: AppColors.label,
+                        height: 16 / 16,
+                        leadingDistribution: TextLeadingDistribution.even,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    CustomTextField(
+                      hintText: 'Your password',
+                      value: loginState.password,
+                      onChanged: (value) {
+                        ref.read(loginProvider.notifier).changePassword(value);
+                      },
+                      textInputAction: TextInputAction.done,
+                      onFieldSubmitted: (value) {
+                        ref.read(loginProvider.notifier).login();
+                      },
+                    ),
+                    const SizedBox(
+                      height: 32,
+                    ),
+                    const Center(
+                      child: Text(
+                        'Forgot password?',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: AppColors.primary,
+                          height: 1,
+                          leadingDistribution: TextLeadingDistribution.even,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 32,
+                    ),
+                    Center(
+                      child: CustomButton(
+                        onPressed: () {
+                          ref.read(loginProvider.notifier).login();
+                        },
+                        text: 'LOGIN',
+                        boxShadow: BoxShadowType.gray,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
