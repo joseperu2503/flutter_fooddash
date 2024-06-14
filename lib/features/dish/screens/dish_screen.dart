995f4486d76bfbd3d8dcd57a5741fda11a524ae4
joseperu2503 <joseperu2503@gmail.com>
2024-06-14 01:49:30 -0500

    dish screen

--- lib/features/dish/screens/dish_screen.dart
+++ lib/features/dish/screens/dish_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:fooddash/config/constants/app_colors.dart';
 import 'package:fooddash/features/dish/models/dish_detail.dart';
 import 'package:fooddash/features/dish/providers/dish_provider.dart';
@@ -94,13 +95,107 @@ class DishScreenState extends ConsumerState<DishScreen> {
         ],
       ),
       bottomNavigationBar: SafeArea(
-        child: SizedBox(
+        child: Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 24,
+          ),
           height: 70,
-          child: Center(
-            child: CustomButton(
-              onPressed: () {},
-              text: 'ADD TO CART',
-            ),
+          child: Row(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: AppColors.primary,
+                      ),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Color(0xffEEF0F2),
+                          offset: Offset(0, 20),
+                          blurRadius: 30,
+                          spreadRadius: 0,
+                        )
+                      ],
+                    ),
+                    height: 40,
+                    width: 40,
+                    child: TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        shape: const OvalBorder(),
+                        padding: EdgeInsets.zero,
+                      ),
+                      child: SvgPicture.asset(
+                        'assets/icons/minus.svg',
+                        height: 20,
+                        colorFilter: const ColorFilter.mode(
+                          AppColors.primary,
+                          BlendMode.srcIn,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    width: 32,
+                    child: const Text(
+                      '1',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: AppColors.black,
+                        height: 1,
+                        leadingDistribution: TextLeadingDistribution.even,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: AppColors.primary,
+                      ),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Color(0xffEEF0F2),
+                          offset: Offset(0, 20),
+                          blurRadius: 30,
+                          spreadRadius: 0,
+                        )
+                      ],
+                    ),
+                    height: 40,
+                    width: 40,
+                    child: TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        shape: const OvalBorder(),
+                        padding: EdgeInsets.zero,
+                      ),
+                      child: SvgPicture.asset(
+                        'assets/icons/plus.svg',
+                        height: 20,
+                        colorFilter: const ColorFilter.mode(
+                          AppColors.primary,
+                          BlendMode.srcIn,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                width: 24,
+              ),
+              Expanded(
+                child: CustomButton(
+                  onPressed: () {},
+                  text: 'ADD TO CART',
+                ),
+              ),
+            ],
           ),
         ),
       ),
--- lib/features/dish/widgets/dish_info.dart
+++ lib/features/dish/widgets/dish_info.dart
@@ -27,83 +27,96 @@ class DishInfo extends StatelessWidget {
               dish.name,
               style: const TextStyle(
                 fontSize: 28,
-                fontWeight: FontWeight.w600,
+                fontWeight: FontWeight.w700,
                 color: AppColors.gray900,
-                height: 1.5,
+                height: 1.2,
+                leadingDistribution: TextLeadingDistribution.even,
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Text(
+              dish.description,
+              style: const TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.w400,
+                color: AppColors.gray600,
+                height: 23 / 15,
                 leadingDistribution: TextLeadingDistribution.even,
               ),
             ),
             const SizedBox(
               height: 12,
             ),
-            Row(
-              children: [
-                Container(
-                  height: 17,
-                  width: 17,
-                  decoration: const BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: Color.fromRGBO(255, 197, 41, 0.4),
-                        offset: Offset(0, 2.94),
-                        blurRadius: 9.81,
-                        spreadRadius: 0,
-                      ),
-                    ],
-                  ),
-                  child: SvgPicture.asset(
-                    'assets/icons/star.svg',
-                    width: 17,
-                    colorFilter: const ColorFilter.mode(
-                      AppColors.yellow,
-                      BlendMode.srcIn,
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  width: 2,
-                ),
-                const Text(
-                  '4.5',
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w600,
-                    color: AppColors.black,
-                    height: 1,
-                    leadingDistribution: TextLeadingDistribution.even,
-                  ),
-                ),
-                const SizedBox(
-                  width: 4,
-                ),
-                const Text(
-                  '(30+)',
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                    color: AppColors.label,
-                    height: 1,
-                    leadingDistribution: TextLeadingDistribution.even,
-                  ),
-                ),
-                const SizedBox(
-                  width: 6,
-                ),
-                const Text(
-                  'See Review',
-                  style: TextStyle(
-                    fontSize: 13,
-                    fontWeight: FontWeight.w400,
-                    color: AppColors.primary,
-                    height: 1,
-                    leadingDistribution: TextLeadingDistribution.even,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 18,
-            ),
+            // Row(
+            //   children: [
+            //     Container(
+            //       height: 17,
+            //       width: 17,
+            //       decoration: const BoxDecoration(
+            //         boxShadow: [
+            //           BoxShadow(
+            //             color: Color.fromRGBO(255, 197, 41, 0.4),
+            //             offset: Offset(0, 2.94),
+            //             blurRadius: 9.81,
+            //             spreadRadius: 0,
+            //           ),
+            //         ],
+            //       ),
+            //       child: SvgPicture.asset(
+            //         'assets/icons/star.svg',
+            //         width: 17,
+            //         colorFilter: const ColorFilter.mode(
+            //           AppColors.yellow,
+            //           BlendMode.srcIn,
+            //         ),
+            //       ),
+            //     ),
+            //     const SizedBox(
+            //       width: 2,
+            //     ),
+            //     const Text(
+            //       '4.5',
+            //       style: TextStyle(
+            //         fontSize: 14,
+            //         fontWeight: FontWeight.w600,
+            //         color: AppColors.black,
+            //         height: 1,
+            //         leadingDistribution: TextLeadingDistribution.even,
+            //       ),
+            //     ),
+            //     const SizedBox(
+            //       width: 4,
+            //     ),
+            //     const Text(
+            //       '(30+)',
+            //       style: TextStyle(
+            //         fontSize: 14,
+            //         fontWeight: FontWeight.w400,
+            //         color: AppColors.label,
+            //         height: 1,
+            //         leadingDistribution: TextLeadingDistribution.even,
+            //       ),
+            //     ),
+            //     const SizedBox(
+            //       width: 6,
+            //     ),
+            //     const Text(
+            //       'See Review',
+            //       style: TextStyle(
+            //         fontSize: 13,
+            //         fontWeight: FontWeight.w400,
+            //         color: AppColors.primary,
+            //         height: 1,
+            //         leadingDistribution: TextLeadingDistribution.even,
+            //       ),
+            //     ),
+            //   ],
+            // ),
+            // const SizedBox(
+            //   height: 18,
+            // ),
             Row(
               children: [
                 const Text(
@@ -126,110 +139,7 @@ class DishInfo extends StatelessWidget {
                     leadingDistribution: TextLeadingDistribution.even,
                   ),
                 ),
-                const Spacer(),
-                Row(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(
-                          color: AppColors.primary,
-                        ),
-                        boxShadow: const [
-                          BoxShadow(
-                            color: Color(0xffEEF0F2),
-                            offset: Offset(0, 20),
-                            blurRadius: 30,
-                            spreadRadius: 0,
-                          )
-                        ],
-                      ),
-                      height: 30,
-                      width: 30,
-                      child: TextButton(
-                        onPressed: () {},
-                        style: TextButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          padding: EdgeInsets.zero,
-                        ),
-                        child: SvgPicture.asset(
-                          'assets/icons/minus.svg',
-                          height: 20,
-                          colorFilter: const ColorFilter.mode(
-                            AppColors.primary,
-                            BlendMode.srcIn,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Container(
-                      alignment: Alignment.center,
-                      width: 32,
-                      child: const Text(
-                        '1',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
-                          color: AppColors.black,
-                          height: 1,
-                          leadingDistribution: TextLeadingDistribution.even,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(
-                          color: AppColors.primary,
-                        ),
-                        color: AppColors.primary,
-                        boxShadow: [
-                          BoxShadow(
-                            color: AppColors.orange.withOpacity(0.4),
-                            offset: const Offset(0, 7),
-                            blurRadius: 15,
-                            spreadRadius: 0,
-                          )
-                        ],
-                      ),
-                      height: 30,
-                      width: 30,
-                      child: TextButton(
-                        onPressed: () {},
-                        style: TextButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          padding: EdgeInsets.zero,
-                        ),
-                        child: SvgPicture.asset(
-                          'assets/icons/plus.svg',
-                          height: 20,
-                          colorFilter: const ColorFilter.mode(
-                            AppColors.white,
-                            BlendMode.srcIn,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
               ],
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            Text(
-              dish.description,
-              style: const TextStyle(
-                fontSize: 15,
-                fontWeight: FontWeight.w400,
-                color: AppColors.gray600,
-                height: 23 / 15,
-                leadingDistribution: TextLeadingDistribution.even,
-              ),
             ),
             const SizedBox(
               height: 30,
