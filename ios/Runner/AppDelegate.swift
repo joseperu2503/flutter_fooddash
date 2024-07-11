b333ebd7a0e0c62e6b095fa8f9ffff335013ba44
joseperu2503 <joseperu2503@gmail.com>
2024-07-11 18:56:52 -0500

    google maps api key ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,14 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    if let path = Bundle.main.path(forResource: "Secrets", ofType: "plist"),
+       let xml = FileManager.default.contents(atPath: path),
+       let secrets = try? PropertyListDecoder().decode([String: String].self, from: xml),
+       let apiKey = secrets["GoogleMapsAPIKey"] {
+      GMSServices.provideAPIKey(apiKey)
+    } else {
+      fatalError("Google Maps API key not found")
+    }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
